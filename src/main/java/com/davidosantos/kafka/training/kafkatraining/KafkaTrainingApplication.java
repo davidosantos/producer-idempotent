1c583aa167c2b658482d0f1199ad8574a8f7de48
davidosantos <davidosantos@hotmail.com>
2022-08-29 21:54:54 -0300

    first commit

---
+++ src/main/java/com/davidosantos/kafka/training/kafkatraining/KafkaTrainingApplication.java
@@ -0,0 +1,104 @@
+package com.davidosantos.kafka.training.kafkatraining;
+
+import java.text.DateFormat;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Properties;
+import java.util.Random;
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+
+@SpringBootApplication
+@EnableScheduling
+public class KafkaTrainingApplication {
+
+	Logger logger = Logger.getLogger(this.toString());
+
+	Properties props = new Properties();
+	KafkaProducer<String, Customers> producer;
+
+	int count = 0;
+
+	private Random random;
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(KafkaTrainingApplication.class, args);
+	}
+
+	@PostConstruct
+	void doingSetups() {
+		logger.info("Doing setups..");
+		this.props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-1:19092,kafka-2:29092,kafka-3:39092");
+		this.props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		this.props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+		this.props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://schema-registry:8081");
+		//this.props.put(ProducerConfig.LINGER_MS_CONFIG, 5000);
+
+		producer = new KafkaProducer<>(props);
+
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			try {
+				this.logger.info("Flusing e Closing the producer...");
+				this.producer.flush();
+				this.producer.close();
+			} catch (Throwable e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}));
+
+	}
+
+	// @Scheduled(fixedRate = 1000l)
+	void updateKafka() {
+		logger.info("Producing messages..");
+//		this.producer.send(new ProducerRecord<String, String>("stream-topic-custom-serdes", "key", "value"));
+
+	}
+
+	@Scheduled(fixedRate = 1000l)
+	void produceMessageWithLambdaCallbacks() {
+		random = new Random();
+		int age = random.ints(33, 36).findAny().getAsInt();
+		int keyIndex = random.ints(0,4).findAny().getAsInt();
+		String keys[] = {"random1", "random2", "random3", "random4"};
+		
+		// ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("stream-topic-custom-serdes", keys[keyIndex],
+		// "{\"name\" : \"David\",\"age\" : "+age+",\"birthDate\" : \"1987-10-09T02:59:00.0000000+00:00\",\"timestamp\" : \""+ Instant.now()+"\"}");
+
+		ProducerRecord<String, Customers> producerRecord = new ProducerRecord<String, Customers>("stream-topic-custom-serdes", keys[keyIndex],
+		Customers.newBuilder()
+		.setName("David")
+		.setAge(age)
+		.setBirthDate(Instant.now().toString())
+		.setTimestamp(Instant.now().toString())
+		.build());
+		
+		logger.info("ProducerRecord: " + producerRecord);
+		
+		producer.send(producerRecord, (recordMetadata, e) -> {
+
+			if (e != null) {
+				e.printStackTrace();
+			} else {
+				logger.info("Message Produced at offset: " + recordMetadata.offset() + " Partition "
+						+ recordMetadata.partition());
+			}
+		});
+	}
+
+}
---
+++ src/test/java/com/davidosantos/kafka/training/kafkatraining/KafkaTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidosantos.kafka.training.kafkatraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
